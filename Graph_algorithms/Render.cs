c0c8ce502b64fe440a3dbb88153805634ca9c84d
Despairon <CBETliy@bigmir.net>
2016-04-18 20:57:33 +0300

    Class "Render" created...

---
+++ Graph_algorithms/Render.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+using System.Windows.Forms;
+
+namespace Graph_algorithms
+{
+    class Render
+    {
+        public Render(ref SimpleOpenGlControl openGL)
+        {
+            graphics = openGL;
+            graphics.InitializeContexts();
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glClearColor(255, 255, 255, 1);
+            Gl.glViewport(0, 0, graphics.Width, graphics.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45, (float)graphics.Width / (float)graphics.Height, 0.1, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+        }
+        SimpleOpenGlControl graphics;
+
+        public void drawCircle(int x, int y)
+        {
+           
+        }
+
+        public void drawArc(int x1, int y1, int x2, int y2)
+        {
+
+        }
+
+        public void drawText(string txt)
+        {
+
+        }
+    }
+}
