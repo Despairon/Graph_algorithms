bfaa3a4afc82e451522eb637dbc6a4cab85d719c
Despairon <CBETliy@bigmir.net>
2016-04-18 17:01:29 +0300

    Creating project...

---
+++ Graph_algorithms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_algorithms
+{
+    public partial class Graph_algorithms : Form
+    {
+        public Graph_algorithms()
+        {
+            InitializeComponent();
+        }
+    }
+}
